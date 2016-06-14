b9a23638d4cf6cc692a2dce3d42268ca73343493
Caleb Evans <caleb@calebevans.me>
2016-06-14 14:31:36 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='alfred-workflow-packager',
+    version='0.8b',
+    description='A CLI utility for packaging and exporting Alfred workflows',
+    url='https://github.com/caleb531/alfred-workflow-packager',
+    author='Caleb Evans',
+    author_email='caleb@calebevans.me',
+    license='MIT',
+    keywords='alfred workflow package export',
+    packages=['awp'],
+    install_requires=[
+        'biplist >= 1, < 2',
+        'jsonschema >= 2, < 3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'alfred-workflow-packager=awp.packager:main',
+            'workflow-packager=awp.packager:main'
+        ]
+    }
+)

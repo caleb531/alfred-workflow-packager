b820a26d1987bc849dca2c9f727a0f6e1fb0ccbe
Caleb Evans <caleb@calebevans.me>
2016-06-13 20:25:43 -0700

    Move all package components into a single module

--- awp.py
+++ awp.py
@@ -2,10 +2,12 @@
 # coding=utf-8
 
 from __future__ import print_function
+import argparse
 import biplist
 import distutils.dir_util as distutils
 import filecmp
 import hashlib
+import json
 import glob
 import plistlib
 import os
@@ -208,3 +210,40 @@ def package_workflow(config, version, export):
             project_path, config['exported_workflow']))
         print('Exported installed workflow successfully (v{})'.format(
             info['version']))
+
+
+# Parse arguments given via command-line interface
+def parse_cli_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'config_path',
+        help='the path to the utility configuration for this project')
+    parser.add_argument(
+        '--export', action='store_true',
+        help='exports the installed workflow to the local project directory')
+    parser.add_argument(
+        '--version',
+        help='the new version number to use for the workflow')
+    return parser.parse_args()
+
+
+# Locate and parse the configuration for the utility
+def get_utility_config(config_path):
+    with open(config_path, 'r') as config_file:
+        return json.load(config_file)
+
+
+def main():
+
+    cli_args = parse_cli_args()
+    config = get_utility_config(cli_args.config_path)
+
+    package_workflow(
+        config,
+        version=cli_args.version,
+        export=cli_args.export)
+
+
+if __name__ == '__main__':
+    main()

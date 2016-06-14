89a91a02ac30361c51bf9ace6523bef6a2290aef
Caleb Evans <caleb@calebevans.me>
2016-06-13 20:39:13 -0700

    Move awp module back into awp package

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 /usr/local/bin/pip2 install biplist jsonschema
-ln -sf "$PWD"/awp.py /usr/local/bin/alfred-workflow-packager
-ln -sf "$PWD"/awp.py /usr/local/bin/workflow-packager
+ln -sf "$PWD"/awp/packager.py /usr/local/bin/alfred-workflow-packager
+ln -sf "$PWD"/awp/packager.py /usr/local/bin/workflow-packager

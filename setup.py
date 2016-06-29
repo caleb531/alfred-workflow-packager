41e44628355c92b5da96a9c3d3a70b7705814ba2
Caleb Evans <caleb@calebevans.me>
2016-06-29 15:37:04 -0700

    Add long description to package setup

--- setup.py
+++ setup.py
@@ -3,10 +3,24 @@
 
 from setuptools import setup
 
+
+# Get long description (used on PyPI project page)
+def get_long_description():
+    try:
+        # Use pandoc to create reStructuredText README if possible
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except:
+        # Otherwise, default to using Markdown README
+        with open('README.md', 'r') as readme_file:
+            return readme_file.read()
+
+
 setup(
     name='alfred-workflow-packager',
     version='0.12.0',
     description='A CLI utility for packaging and exporting Alfred workflows',
+    long_description=get_long_description(),
     url='https://github.com/caleb531/alfred-workflow-packager',
     author='Caleb Evans',
     author_email='caleb@calebevans.me',
